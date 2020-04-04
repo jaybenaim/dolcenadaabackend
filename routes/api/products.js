bebc99a97eb5851ec1be23fef4532a8b884206c1
jaybenaim <benaimjacob@gmail.com>
2020-04-04 18:58:27 -0400

    added routes to get indidvidual items by name or sku

--- routes/api/products.js
+++ routes/api/products.js
@@ -7,6 +7,7 @@ require("dotenv").config();
 
 router.get("/", (req, res) => {
   Product.find()
+    .select("-__v")
     .then(product => {
       return res.status(200).send(product);
     })
@@ -46,34 +47,62 @@ router.post("/createProductList", (req, res) => {
 
 // GET BY ID
 router.get("/:id", (req, res) => {
-  Product.findOne({ _id: req.params.id }, (err, product) => {
-    if (err) return res.status(500).send(err);
-    return res.status(200).send(product);
-  });
+  Product.findOne({ _id: req.params.id })
+    .select("-__v")
+    .then(product => {
+      return res.status(200).send(product);
+    })
+    .catch(err => {
+      return res.status(500).send(err);
+    });
 });
 // GET COOKIES
-router.get("/cookies", (req, res) => {
-  Product.find({ sku: "cookie-" })
-    .then(cookie => {
-      return res.status(200).send(cookie);
+router.get("/get/cookies", (req, res) => {
+  const skuQuery = { sku: "cookie-" };
+  const nameQuery = { name: req.query.name };
+  let query;
+  if (req.query.name) {
+    query = nameQuery;
+  } else {
+    query = skuQuery;
+  }
+  Product.find(query)
+    .then(cookies => {
+      return res.status(200).send(cookies);
     })
     .catch(err => {
       return res.status(500).send(err);
     });
 });
 // GET SCONES
-router.get("/scones", (req, res) => {
-  Product.find({ sku: "scone-" })
-    .then(scone => {
-      return res.status(200).send(scone);
+router.get("/get/scones", (req, res) => {
+  const skuQuery = { sku: "scone-" };
+  const nameQuery = { name: req.query.name };
+  let query;
+  if (req.query.name) {
+    query = nameQuery;
+  } else {
+    query = skuQuery;
+  }
+  Product.find(query)
+    .then(scones => {
+      return res.status(200).send(scones);
     })
     .catch(err => {
       return res.status(500).send(err);
     });
 });
 // GET BISCOTI
-router.get("/biscotti", (req, res) => {
-  Product.find({ sku: "biscotti-" })
+router.get("/get/biscotti", (req, res) => {
+  const skuQuery = { sku: "biscotti-" };
+  const nameQuery = { name: req.query.name };
+  let query;
+  if (req.query.name) {
+    query = nameQuery;
+  } else {
+    query = skuQuery;
+  }
+  Product.find(query)
     .then(biscotti => {
       return res.status(200).send(biscotti);
     })
